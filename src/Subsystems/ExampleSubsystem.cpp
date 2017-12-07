035f4d98d308659d411bc4007905a43ff0fef678
Benjamin Davis <greendartfrog@gmail.com>
2017-12-06 22:52:33 -0500

    Added RobotBuilder framework

---
+++ src/Subsystems/ExampleSubsystem.cpp
@@ -0,0 +1,18 @@
+#include "../RobotMap.h"
+#include "../../../../git/Training_Board/src/Subsystems/ExampleSubsystem.h"
+
+#include "../../../../git/Training_Board/src/RobotMap.h"
+
+ExampleSubsystem::ExampleSubsystem() :
+		frc::Subsystem("ExampleSubsystem") {
+
+}
+
+void ExampleSubsystem::InitDefaultCommand() {
+	// Set the default command for a subsystem here.
+	// SetDefaultCommand(new MySpecialCommand());
+}
+
+// Put methods for controlling this subsystem
+// here. Call these from Commands.
+
